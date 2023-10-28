dd929941e7327fb1aec22754af9d57961d97dccb
Eigensystem <eigensystem1318@gmail.com>
2023-10-28 11:42:37 +0800

    fix: baseline

--- src/baseline/apsp.cc
+++ src/baseline/apsp.cc
@@ -1,13 +1,13 @@
 #include "graph.hh"
 
 Graph Graph::apsp() {
-//    Graph result(*this);
-//    for (int k = 0; k < vertex_num_; ++k) {
-//        for (int i = 0; i < vertex_num_; ++i) {
-//            for (int j = 0; j < vertex_num_; ++j) {
-//                result(i, j) = std::min(result(i, j), result(i, k) + result(k, j));
-//            }
-//        }
-//    }
-//    return result;
+   Graph result(*this);
+   for (int k = 0; k < vertex_num_; ++k) {
+       for (int i = 0; i < vertex_num_; ++i) {
+           for (int j = 0; j < vertex_num_; ++j) {
+               result(i, j) = std::min(result(i, j), result(i, k) + result(k, j));
+           }
+       }
+   }
+   return result;
 }
