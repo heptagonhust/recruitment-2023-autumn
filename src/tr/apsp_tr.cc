00fc560124b233b431f97fdf7ea6ccd7efd7ed13
tr <u202215573@hust.edu.cn>
2023-12-09 23:09:09 +0800

    "openMP1"

--- src/tr/apsp_tr.cc
+++ src/tr/apsp_tr.cc
@@ -1,22 +1,43 @@
 #include "graph.hh"
+#include <omp.h>
 
-Graph Graph::apsp() {
+// Graph Graph::apsp() {
+//     Graph result(*this);
+//     int *Distance = result.get_raw_ptr();
+//     int ij = 0, ik = 0, kj = 0;
+//     for (int k = 0; k < vertex_num_; ++k) {
+//         ij = 0;
+//         ik = k;
+//         for (int i = 0; i < vertex_num_; ++i) {
+//             kj = k * vertex_num_;
+//             int Distance_ik = Distance[ik];
+//             for (int j = 0; j < vertex_num_; ++j) {
+//                 result(i, j) = std::min(Distance[ij], Distance_ik + Distance[kj]);
+//                 ++ij;
+//                 ++kj;
+//             }
+//             ik += vertex_num_;
+//         }
+//     }
+//     return result;
+// }
+
+Graph Graph::apsp()
+{
     Graph result(*this);
-    int *Distance = result.get_raw_ptr();
-    int ij = 0, ik = 0, kj = 0;
-    for (int k = 0; k < vertex_num_; ++k) {
-        ij = 0;
-        ik = k;
-        for (int i = 0; i < vertex_num_; ++i) {
-            kj = k * vertex_num_;
-            int Distance_ik = Distance[ik];
-            for (int j = 0; j < vertex_num_; ++j) {
-                result(i, j) = std::min(Distance[ij], Distance_ik + Distance[kj]);
-                ++ij;
-                ++kj;
+
+#pragma omp parallel for collapse(2)
+    for (int k = 0; k < vertex_num_; ++k)
+    {
+#pragma omp parallel for
+        for (int i = 0; i < vertex_num_; ++i)
+        {
+            for (int j = 0; j < vertex_num_; ++j)
+            {
+                result(i, j) = std::min(result(i, j), result(i, k) + result(k, j));
             }
-            ik += vertex_num_;
         }
     }
+
     return result;
 }
